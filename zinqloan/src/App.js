3e07c4670b2b6ca07e1fff01e9e94e01f4341438
pratikbhumkar <prateekbhumkar@gmail.com>
2018-12-25 18:30:45 +1100

    added form controls and visual improvements

--- zinqloan/src/App.js
+++ zinqloan/src/App.js
@@ -1,27 +1,56 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Form, FormGroup,Checkbox,FormControl,ControlLabel, Button } from 'react-bootstrap' // add more if you are using more components from bootstrap
+
+const formInstance=(
+
+  <div className="login" >
+  <h1><center>Zinq</center></h1>
+        <form style={{textAlign:"center"}}>
+          <FormGroup controlId="email" bsSize="large"  >
+            <ControlLabel style={{marginRight:45}}>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value="Email Id"
+            />
+          </FormGroup>
+          <br></br>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel style={{marginRight:20}}>Password</ControlLabel>
+            <FormControl
+              value="Password Here"
+              type="password"
+            />
+          </FormGroup>
+          <br></br>
+          <br></br>
+          <Button
+            block
+            bsSize="large"
+            // disabled={!this.validateForm()}
+            type="submit"
+            style={{marginRight:70, width: 120}}
+          >
+            Login
+          </Button>
+          <Button
+            block
+            bsSize="large"
+            // disabled={!this.validateForm()}
+            type="create"
+            style={{ width: 120}}
+          >
+            Create Account
+          </Button>
+        </form>
+      </div>
+)
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return (formInstance);
   }
 }
 
